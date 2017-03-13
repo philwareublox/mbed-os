b242900a5b712573796519d68350166f5e93bdb7
Rob Meades <rob.meades@u-blox.com>
2017-03-13 15:48:40 +0000

    c030_api.h => C030_api.h

--- targets/TARGET_STM/TARGET_STM32F4/TARGET_UBLOX_C030/device/system_stm32f4xx.c
+++ targets/TARGET_STM/TARGET_STM32F4/TARGET_UBLOX_C030/device/system_stm32f4xx.c
@@ -80,7 +80,7 @@
 
 #include "stm32f4xx.h"
 #include "hal_tick.h"
-#include "c030_api.h"
+#include "C030_api.h"
 
 #if !defined  (HSE_VALUE) 
   #define HSE_VALUE    ((uint32_t)8000000) /*!< Default value of the External oscillator in Hz */

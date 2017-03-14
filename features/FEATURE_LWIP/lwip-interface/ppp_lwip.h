3580674dffaf7fd28a8dfdec2a216eac8461f26e
Hasnain Virk <hasnain.virk@arm.com>
2017-03-14 17:20:56 +0200

    Adding dummies for ppp methods
    If PPP is not used, we provide dummy functions.

--- features/FEATURE_LWIP/lwip-interface/ppp_lwip.h
+++ features/FEATURE_LWIP/lwip-interface/ppp_lwip.h
@@ -3,13 +3,19 @@
  */
 #ifndef PPP_LWIP_H_
 #define PPP_LWIP_H_
-
+#include "netif/ppp/pppapi.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
+#if LWIP_PPP_API
 nsapi_error_t ppp_lwip_if_init(struct netif *netif);
 err_t ppp_lwip_connect(void);
 err_t ppp_lwip_disconnect(void);
+#else
+#define ppp_lwip_if_init(netif)     NSAPI_ERROR_UNSUPPORTED
+#define ppp_lwip_connect()          ERR_IF
+#define ppp_lwip_disconnect()       ERR_IF
+#endif //LWIP_PPP_API
 #ifdef __cplusplus
 }
 #endif
